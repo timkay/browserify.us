00fb9adea8f41c60e8ec0056b7340f35febd04ac
timkay <tim@synsec.ai>
2022-01-06 20:35:30 -0800

    reworked to be compatible with render.com

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const {mkdirSync, appendFileSync, existsSync} = require('fs')
 const {spawn, spawnSync} = require('child_process')
+const process = require('process')
 
 const express = require('express')
 const app = express()
@@ -19,39 +20,51 @@ try {
 
 
 app.get('/', (req, res) => {
-	res.type('text/plain')
-	res.send(usage)
+    res.type('text/plain')
+    res.send(usage)
 })
 
 app.get('/require/:data', (req, res) => {
-	const data = req.params.data
-	const file = `data/${data}`
-	if (existsSync(file)) {
-		res.type('text/javascript')
-		return res.sendFile(file, {root: __dirname})
-	}
-	let packages = []
-	let variables = []
-	data.split(/\+/).forEach(module => {
-		const m = module.match(/^(\w+)(?:=([\w\-]+)(\@[\w\.]+)?)?$/)
-		if (m && m.length === 4) {
-			let [__, variable, package = variable, version = ''] = m
-			variables.push(`${variable} = require("${package}")`)
-			packages.push(package + version)
-		}
-	})
-	if (packages.length === 0) {
-		res.type('text/plain')
-		res.send(usage)
-	}
-	spawnSync('/bin/bash', ['-c', ['npm install', ...packages].join(' ')])
-	let cmd = `echo '${variables.join('\n')}' |node_modules/.bin/browserify - |tee ${file}`
-	appendFileSync('data/log.txt', `${cmd}\n`, 'utf8') 
-	let child = spawn('/bin/bash', ['-c', cmd])
+    const data = req.params.data
+    const file = `data/${data}`
+    if (existsSync(file)) {
 	res.type('text/javascript')
-	return child.stdio[1].pipe(res)
+	return res.sendFile(file, {root: __dirname})
+    }
+    let packages = []
+    let variables = []
+    data.split(/,/).forEach(module => {
+	const m = module.match(/^(\w+)(?:=([\w\-]+)(\@[\w\.]+)?)?$/)
+	if (m && m.length === 4) {
+	    let [__, variable, package = variable, version = ''] = m
+	    variables.push(`${variable} = require("${package}")`)
+	    packages.push(package + version)
+	}
+    })
+    if (packages.length === 0) {
+	res.type('text/plain')
+	res.send(usage)
+    }
+    spawnSync('npm', ['install', ...packages])
+    let cmd = `echo '${variables.join('\n')}' |node_modules/.bin/browserify - |tee ${file}`
+    appendFileSync('data/log.txt', `${cmd}\n`, 'utf8') 
+    let child = spawn('/bin/ash', ['-c', cmd])
+    res.type('text/javascript')
+    return child.stdio[1].pipe(res)
 })
 
-app.listen(port, () => {
-	console.log(`Listening at http://localhost:${port}`)
+const server = app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
+})
+
+process.on('SIGINT', () => {
+    server.close(() => {
+        console.log('\nSIGINT: HTTP server closed');
+    });
+})
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('SIGKILL: HTTP server closed');
+    });
 })
